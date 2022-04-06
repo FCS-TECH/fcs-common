789dfc49e0fe1231a07880847ace07c0b2ca8542
FH <fh@uex.dk>
2022-04-06 12:03:28 +0200

    added base vat info class

---
+++ BaseVatInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace FCS.Lib.Common
+{
+    public class BaseVatInfo
+    {
+        public string Name { get; set; }
+        public string CoName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string VatNumber { get; set; }
+        public int VatNumberValid { get; set; }
+        public int HasFolded  { get; set; }
+        public string RequestDate { get; set; }
+    }
+}
